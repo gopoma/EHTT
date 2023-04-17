584c800cb72d420785927dbb39c13cee5fe669a0
gopoma <gordono@unsa.edu.pe>
2023-04-17 17:08:29 -0500

    feat: selectedFields in PeopleTable

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,10 +1,17 @@
-import { useState, type FC, useEffect } from 'react'
+import React, { useState, type FC, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import { ArrowDownIcon, ArrowUpIcon } from './icons'
 import { useSorting } from '../hooks'
 import { type Person } from '../interfaces'
 import { getPeople } from '../services'
+
+const selectedFields: Partial<Record<keyof Person, boolean>> = {
+  category: true,
+  company: true,
+  happinessLevel: true,
+  name: true
+}
 
 export const PeopleTable: FC = () => {
   const [people, setPeople] = useState<Person[]>([])
@@ -30,45 +37,55 @@ export const PeopleTable: FC = () => {
         <thead>
           <tr>
             {
-              Object.keys(people[0]).map((key) => (
-                <th
-                  key={ key }
-                  className='p-2 border border-black'
-                >
-                  <section className='flex gap-2 place-items-center'>
-                    <span>{ key }</span>
-                    <span
-                      onClick={onSortingCriteriaElementClick(key as keyof Person)}
-                      className='cursor-pointer'
-                    >
-                      {
-                        (sortingCriterias[key as keyof Person])
-                          ? <ArrowUpIcon style={{ width: '15px', height: '15px' }} />
-                          : <ArrowDownIcon style={{ width: '15px', height: '15px' }} />
-                      }
-                    </span>
-                  </section>
-                </th>
-              ))
+              Object.keys(people[0]).map((key) => {
+                if (!(selectedFields[key as keyof Person] as boolean)) {
+                  return <React.Fragment key={ key }></React.Fragment>
+                }
+
+                return (
+                  <th
+                    key={ key }
+                    className='p-2 border border-black'
+                  >
+                    <section className='flex gap-2 place-items-center'>
+                      <span>{ key }</span>
+                      <span
+                        onClick={onSortingCriteriaElementClick(key as keyof Person)}
+                        className='cursor-pointer'
+                      >
+                        {
+                          (sortingCriterias[key as keyof Person])
+                            ? <ArrowUpIcon style={{ width: '15px', height: '15px' }} />
+                            : <ArrowDownIcon style={{ width: '15px', height: '15px' }} />
+                        }
+                      </span>
+                    </section>
+                  </th>
+                )
+              })
             }
           </tr>
         </thead>
         <tbody>
             {
-              sortedItems.map((sortedItem) => (
-                <tr key={ uuidv4() }>
+              sortedItems.map((sortedItem) => {
+                return (<tr key={ uuidv4() }>
                   {
-                    Object.values(sortedItem).map((value) => (
-                      <td
+                    Object.entries(sortedItem).map(([key, value]) => {
+                      if (!(selectedFields[key as keyof Person] as boolean)) {
+                        return <React.Fragment key={ key }></React.Fragment>
+                      }
+
+                      return (<td
                         key={ uuidv4() }
                         className='p-2 border border-black'
                       >
                         { value }
-                      </td>
-                    ))
+                      </td>)
+                    })
                   }
-                </tr>
-              ))
+                </tr>)
+              })
             }
         </tbody>
       </table>
