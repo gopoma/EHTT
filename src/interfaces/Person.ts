ed3be5e7fc4d7bb282beda4adbbb1770f98b7b21
gopoma <gordono@unsa.edu.pe>
2023-04-16 22:58:06 -0500

    add initial boilerplate

---
+++ src/interfaces/Person.ts
@@ -0,0 +1,7 @@
+export interface Person {
+  name: string
+  category: 'Employee' | 'Manager'
+  company: string
+  companyLogo: string
+  happinessLevel: number
+}
---
+++ src/mocks/persons.ts
@@ -0,0 +1,88 @@
+import type { Person } from '../interfaces'
+
+export const people: Person[] = [
+  {
+    name: 'Alice',
+    category: 'Employee',
+    company: 'Google',
+    companyLogo: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+    happinessLevel: 80
+  },
+  {
+    name: 'Bob',
+    category: 'Manager',
+    company: 'Microsoft',
+    companyLogo: 'https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/logo.aspx',
+    happinessLevel: 70
+  },
+  {
+    name: 'Charlie',
+    category: 'Employee',
+    company: 'Amazon',
+    companyLogo: 'https://www.amazon.com/gp/help/customer/display.html?nodeId=202029230',
+    happinessLevel: 60
+  },
+  {
+    name: 'Dave',
+    category: 'Manager',
+    company: 'Apple',
+    companyLogo: 'https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?202204040805',
+    happinessLevel: 90
+  },
+  {
+    name: 'Eve',
+    category: 'Employee',
+    company: 'IBM',
+    companyLogo: 'https://www.ibm.com/brand/systems/ww/assets/brand-guidelines/downloads/ibm-brand-design-identity-quick-reference-guide.pdf',
+    happinessLevel: 80
+  },
+  {
+    name: 'Frank',
+    category: 'Manager',
+    company: 'Cisco',
+    companyLogo: 'https://www.cisco.com/c/dam/global/en_us/about/ac48/assets/images/Cisco_logo.png',
+    happinessLevel: 70
+  },
+  {
+    name: 'Grace',
+    category: 'Employee',
+    company: 'Facebook',
+    companyLogo: 'https://en.facebookbrand.com/facebookapp/assets/f-logo-rgb-100.png',
+    happinessLevel: 60
+  },
+  {
+    name: 'Harry',
+    category: 'Manager',
+    company: 'Intel',
+    companyLogo: 'https://www.intel.com/content/dam/www/public/us/en/images/meta-images/intel-logo.png',
+    happinessLevel: 90
+  },
+  {
+    name: 'Irene',
+    category: 'Employee',
+    company: 'Oracle',
+    companyLogo: 'https://www.oracle.com/a/ocom/img/cb71-1-3.png',
+    happinessLevel: 80
+  },
+  {
+    name: 'Jack',
+    category: 'Manager',
+    company: 'HP',
+    companyLogo: 'https://ssl.www8.hp.com/emea_africa_middle_east/en/images/HP_logo_3D.png',
+    happinessLevel: 70
+  },
+  {
+    name: 'Kate',
+    category: 'Employee',
+    company: 'Dell',
+    companyLogo: 'https://www.delltechnologies.com/content/dam/delltechnologies/images/logos/logo-dell-100px.png',
+    happinessLevel: 50
+  },
+  {
+    name: 'Liam',
+    category: 'Manager',
+    company: 'VMware',
+    companyLogo: 'https://www.vmware.com/content/dam/digitalmarketing/vmware/en/images/meta/vmware-logo.jpg',
+    happinessLevel: 80
+  }
+]
