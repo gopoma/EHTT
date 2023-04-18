5923d0b26c87998be15d765b747ac5f4ac316e9a
gopoma <gordono@unsa.edu.pe>
2023-04-17 19:23:50 -0500

    feat: add searching

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type FC, useEffect } from 'react'
+import React, { useState, type FC, useEffect, type ChangeEvent } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import { ArrowDownIcon, ArrowUpIcon } from './icons'
@@ -27,12 +27,84 @@ export const PeopleTable: FC = () => {
       .catch(console.log)
   }, [])
 
+  interface SearchFilterCriteria {
+    field: string
+    value: string
+  }
+  const initialSearchFilterCriteria: SearchFilterCriteria = {
+    field: '',
+    value: ''
+  }
+  const [searchFilterCriteria, setSearchFilterCriteria] = useState<SearchFilterCriteria>(initialSearchFilterCriteria)
+  const [searchResults, setSearchResults] = useState<Person[]>(sortedItems)
+
+  const onSearchFilterFieldChange = ({ target: { value: searchFilterField } }: ChangeEvent<HTMLSelectElement>): void => {
+    setSearchFilterCriteria((prevSearchFilterCriteria) => ({
+      ...prevSearchFilterCriteria,
+      field: searchFilterField
+    }))
+  }
+
+  const onSearchFilterValueChange = ({ target: { value: searchFilterValue } }: ChangeEvent<HTMLInputElement>): void => {
+    setSearchFilterCriteria((prevSearchFilterCriteria) => ({
+      ...prevSearchFilterCriteria,
+      value: searchFilterValue
+    }))
+  }
+
+  useEffect(() => {
+    const { field, value } = searchFilterCriteria
+
+    if (field === '' || value === '') {
+      setSearchResults([])
+      return
+    }
+
+    const newSearchResults = sortedItems.filter((sortedItem) => sortedItem[field as keyof Person].toString().toLowerCase().includes(value.toLowerCase()))
+
+    setSearchResults(newSearchResults)
+  }, [searchFilterCriteria])
+
+  useEffect(() => {
+  }, [searchResults])
+
   if (sortedItems.length === 0) {
     return <></>
   }
 
   return (
-    <>
+    <section className='flex flex-col gap-4 p-4'>
+      <section className='flex flex-col gap-2'>
+        <label className='text-xl font-bold'>Search:</label>
+        <section className='flex gap-2'>
+          <select
+            onChange={onSearchFilterFieldChange}
+            className='w-[12%] p-2 bg-sky-100 border border-sky-800'
+          >
+            {
+              <>
+                <option value=''>Choose a Field</option>
+                {
+                  Object.keys(people[0]).map((key) => (
+                    <option
+                      key={ key }
+                      value={ key }
+                    >
+                      { key }
+                    </option>
+                  ))
+                }
+              </>
+            }
+          </select>
+          <input
+            type='text'
+            className='w-[25%] border border-black py-2 px-3'
+            value={searchFilterCriteria.value}
+            onChange={onSearchFilterValueChange}
+          />
+        </section>
+      </section>
       <table>
         <thead>
           <tr>
@@ -68,7 +140,7 @@ export const PeopleTable: FC = () => {
         </thead>
         <tbody>
             {
-              sortedItems.map((sortedItem) => {
+              (searchResults.length !== 0 ? searchResults : sortedItems).map((sortedItem) => {
                 return (<tr key={ sortedItem.id }>
                   {
                     Object.entries(sortedItem).map(([key, value]) => {
@@ -89,6 +161,6 @@ export const PeopleTable: FC = () => {
             }
         </tbody>
       </table>
-    </>
+    </section>
   )
 }
