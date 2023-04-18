02d07b3f7c4371c1458e88afcb8875ff08ddb361
gopoma <gordono@unsa.edu.pe>
2023-04-17 22:14:27 -0500

    remove unnecesary React import

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState, type FC, useEffect } from 'react'
+import { type FC, useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import { ArrowDownIcon, ArrowUpIcon } from './icons'
